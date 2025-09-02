fcb62b54d348904c89928d75f6f12c7315b565c6
Piyush Chauhan <piyushchauhan70900@gmail.com>
2025-09-02 18:42:47 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import Login  from "./components/Auth/Login";
 import Employees from "./components/Dashboard/Employees";
+import Admin from "./components/Dashboard/Admin";
 function App() {
   return (
     <>
       {/* <Login></Login> */}
-      <Employees></Employees>
+      {/* <Employees></Employees> */}
+      <Admin></Admin>
+
     </>
   );
 }
